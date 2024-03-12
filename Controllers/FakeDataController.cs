5cd582c05b64535cfba9623514836d9cd5b25d3e
papaveeskas <ppwsks.forwork@gmail.com>
2024-03-13 02:32:53 +0300

    fix

--- Controllers/FakeDataController.cs
+++ Controllers/FakeDataController.cs
@@ -12,31 +12,31 @@ namespace StroiTrendTest.Controllers
         [HttpGet("totalchats")]
         public ActionResult<TotalChatsReport<ChatData>> GetTotalChats()
         {
-            return Ok(System.IO.File.ReadAllText("data/total-chats-report.json"));
+            return Ok(System.IO.File.ReadAllText("Data/total-chats-report.json"));
         }
 
         [HttpGet("durationdata")]
         public ActionResult<TotalChatsReport<DurationData>> GetDuration()
         {
-            return Ok(System.IO.File.ReadAllText("data/duration-report.json"));
+            return Ok(System.IO.File.ReadAllText("Data/duration-report.json"));
         }
 
         [HttpGet("ratingsdata")]
         public ActionResult<TotalChatsReport<RatingsData>> GetRatings()
         {
-            return Ok(System.IO.File.ReadAllText("data/ratings-report.json"));
+            return Ok(System.IO.File.ReadAllText("Data/ratings-report.json"));
         }
 
         [HttpGet("responsetimedata")]
         public ActionResult<TotalChatsReport<ResponseTimeData>> GetResponseTime()
         {
-            return Ok(System.IO.File.ReadAllText("data/response-time-report.json"));
+            return Ok(System.IO.File.ReadAllText("Data/response-time-report.json"));
         }
 
         [HttpGet("tags")]
         public ActionResult<TotalChatsReport<TagsData>> GetTags()
         {
-            return Ok(System.IO.File.ReadAllText("data/tags-report.json"));
+            return Ok(System.IO.File.ReadAllText("Data/tags-report.json"));
         }
     }
 }
